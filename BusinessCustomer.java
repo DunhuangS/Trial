f032a92aa4b89fbb71ccc6a234d6f297b8989ad3
DunhuangS <2003358@apps.nsd.org>
2026-01-28 09:08:35 -0800

    more uses for this() and learning super() with inheritance

---
+++ BusinessCustomer.java
@@ -0,0 +1,8 @@
+public class BusinessCustomer extends Customer {
+    
+
+    public BusinessCustomer(String firstName, String lastName, Address address, Phone phone, String companyName, int taxId){
+        super(firstName, lastName, address, phone);
+
+    }
+}
--- Customer.java
+++ Customer.java
@@ -3,12 +3,15 @@
 public class Customer {
 
     private long id;
-    
+
     private String firstName;
     private String lastName;
 
     private Address address;
     private Phone phone;
+    public Customer() {
+        this("Anon", "Customer", new Address(), new Phone());
+    }
 
     public Customer(String firstName, String lastName, Address a, Phone p) {
         this.firstName = firstName;
